1a747da73a4d014536497b5679a5cce0e42b624f
AnahiRuizCalderon <301710458@bis.ute.edu.mx>
2019-06-07 07:33:33 -0500

    Se agregó el Login, la Página Principal y la página de Maestros (cada una con sus respectivos ViewModels, Controller y Vista)

---
+++ TutoriasUTE/ViewModels/MaestrosResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TutoriasUTE.ViewModels
+{
+    public class MaestrosResultViewModel
+    {
+        [Display(Name = "Maestros")]
+        public List<MaestrosViewModel> Maestros { get; set; }
+    }
+}
